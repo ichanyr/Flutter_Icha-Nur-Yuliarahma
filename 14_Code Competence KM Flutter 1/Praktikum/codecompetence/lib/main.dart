d7c1d594da4cdb90b07f32368c517b7b525adc02
ichayr <ichayuliarahma81@gmail.com>
2023-10-01 21:36:17 +0700

    code competence 1

--- 14_Code Competence KM Flutter 1/Praktikum/codecompetence/lib/main.dart
+++ 14_Code Competence KM Flutter 1/Praktikum/codecompetence/lib/main.dart
@@ -4,22 +4,193 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var formKey = GlobalKey<FormState>();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController textAreaController = TextEditingController();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Threads'),
-          centerTitle: true,
-          leading: Icon(Icons.menu_book_rounded),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.purple[300],
+            title: Text('Code Competence 1'),
+            centerTitle: true,
+            leading: Icon(Icons.menu_book_rounded),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/cc2.png',
+                      height: 150,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Contact Us',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            '''Need to get in touch with us? Either fill out the form with your inquiry or find the department email you'd like to contact below. ''',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 46,
+                ),
+                Stack(
+                  children: [
+                    Image.asset('assets/images/cc2.png',
+                        height: 200, width: double.infinity),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Positioned(
+                                top: 20,
+                                left: 20,
+                                child: Container(
+                                  color: Colors.grey[200],
+                                  width: 161,
+                                  child: TextFormField(
+                                    controller: firstNameController,
+                                    decoration: InputDecoration(
+                                      labelText: 'First Name',
+                                      border: OutlineInputBorder(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Positioned(
+                                top: 20,
+                                left: 20,
+                                child: Container(
+                                  color: Colors.grey[200],
+                                  width: 161,
+                                  child: TextFormField(
+                                    controller: lastNameController,
+                                    decoration: InputDecoration(
+                                      labelText: 'Last Name',
+                                      border: OutlineInputBorder(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 20,
+                            child: Container(
+                              color: Colors.grey[200],
+                              width: double.maxFinite,
+                              child: TextFormField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 20,
+                            child: Container(
+                              color: Colors.grey[200],
+                              width: double.maxFinite,
+                              child: TextField(
+                                maxLines: 3,
+                                controller: textAreaController,
+                                decoration: InputDecoration(
+                                  labelText: 'What can we help you with?',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Data'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('First Name: ${firstNameController.text}'),
+                              Text('Last Name: ${lastNameController.text}'),
+                              Text('Email: ${emailController.text}'),
+                              Text('Address: ${textAreaController.text}'),
+                            ],
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
